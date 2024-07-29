43dace61876fd050150ec4ab305d15eee09f2d1c
Rock Clapps <rockclapps@gmail.com>
2024-07-29 14:28:57 -0400

    Actually fixed it this time

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -5,31 +5,24 @@
  ;; If there is more than one, they won't work right.
  '(auto-save-default nil)
  '(auto-save-visited-mode t)
- '(custom-enabled-themes '(monokai))
+ '(custom-enabled-themes '(zenburn))
  '(custom-safe-themes
-   '("95b0bc7b8687101335ebbf770828b641f2befdcf6d3c192243a251ce72ab1692" default))
+   '("f366d4bc6d14dcac2963d45df51956b2409a15b770ec2f6d730e73ce0ca5c8a7" default))
  '(delete-selection-mode nil)
  '(evil-undo-system 'undo-redo)
  '(global-display-line-numbers-mode t)
- '(global-whitespace-mode t)
  '(indent-tabs-mode nil)
  '(inhibit-startup-screen t)
  '(initial-scratch-message nil)
  '(kill-buffer-delete-auto-save-files nil)
  '(make-backup-files nil)
  '(package-selected-packages
-   '(markdown-mode go-mode which-key ## company evil-surround monokai-theme magit evil))
+   '(zenburn-theme markdown-mode go-mode which-key ## company evil-surround magit evil))
  '(pixel-scroll-precision-mode t)
  '(savehist-mode t)
  '(standard-indent 2)
  '(tab-always-indent t)
- '(tool-bar-mode nil)
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- ))
+ '(tool-bar-mode nil))
 
 (add-hook 'text-mode-hook 'auto-fill-mode)
 (add-hook 'text-mode-hook 'flyspell-mode)
@@ -46,3 +39,9 @@
 ;; (which-key-mode)
 
 ;; To recompile all .el files, run C-u 0 M-x byte-recompile-directory
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
