705b7665da8ef3e61e0fec811a1ba7e7fb0ba90a
Rock Clapps <rockclapps@gmail.com>
2024-08-20 18:47:15 -0400

    organized " and '

--- .bash_aliases
+++ .bash_aliases
@@ -1,24 +1,24 @@
-alias l="ls -AGXhl --group-directories-first --color=auto"
-alias xyeys="xeyes"
-alias hackswitch="sudo python3 ~/MEGA/Game\ Saves/Switch\ Hacking/fusee-launcher-1.0/fusee-launcher.py ~/MEGA/Game\ Saves/Switch\ Hacking/fusee-launcher-1.0/pls/payload.bin"
-alias switchloader="sudo -E java -jar ~/MEGA/Game\ Saves/Switch\ Hacking/ns-usbloader-7.1.jar"
-alias suedit="sudo nvim -u ~/.config/nvim/init-compat"
+alias l='ls -AGXhl --group-directories-first --color=auto'
+alias xyeys='xeyes'
+alias hackswitch='sudo python3 ~/MEGA/Game\ Saves/Switch\ Hacking/fusee-launcher-1.0/fusee-launcher.py ~/MEGA/Game\ Saves/Switch\ Hacking/fusee-launcher-1.0/pls/payload.bin'
+alias switchloader='sudo -E java -jar ~/MEGA/Game\ Saves/Switch\ Hacking/ns-usbloader-7.1.jar'
+alias suedit='sudo nvim -u ~/.config/nvim/init-compat'
 alias dotfiles='git --git-dir="$HOME/.dotfiles" --work-tree="$HOME"'
 alias lazydotfiles='lazygit -g "$HOME/.dotfiles" -w "$HOME"'
 
-alias gstat="git status"
-alias gcom="git commit -m"
-alias gpush="git push"
-alias gpull="git pull"
+alias gstat='git status'
+alias gcom='git commit -m'
+alias gpush='git push'
+alias gpull='git pull'
 
-alias flati="sudo flatpak install"
-alias flatr="sudo flatpak remove --delete-data"
-alias flatu="sudo flatpak update"
-alias flats="flatpak search"
+alias flati='sudo flatpak install'
+alias flatr='sudo flatpak remove --delete-data'
+alias flatu='sudo flatpak update'
+alias flats='flatpak search'
 
-alias paci="sudo pacman -S"
-alias pacr="sudo pacman -Rsn"
-alias pacu="sudo pacman -Syu"
-alias pacs="pacman -Ss"
+alias paci='sudo pacman -S'
+alias pacr='sudo pacman -Rsn'
+alias pacu='sudo pacman -Syu'
+alias pacs='pacman -Ss'
 
-alias update="pacu && flatu"
+alias update='pacu && flatu'
--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,21 +1,21 @@
-vim.opt.langmap="hjklHJKLneioNEIO;neioNEIOhjklHJKL"
-vim.keymap.set("n", ";", ":")
-vim.keymap.set({"t", "i"}, "<A-h>", "<C-\\><C-N><C-w>h")
-vim.keymap.set({"t", "i"}, "<A-j>", "<C-\\><C-N><C-w>j")
-vim.keymap.set({"t", "i"}, "<A-k>", "<C-\\><C-N><C-w>k")
-vim.keymap.set({"t", "i"}, "<A-l>", "<C-\\><C-N><C-w>l")
-vim.keymap.set({"t", "i"}, "<A-h>", "<C-\\><C-N><C-w>h")
-vim.keymap.set({"t", "i"}, "<A-j>", "<C-\\><C-N><C-w>j")
-vim.keymap.set({"t", "i"}, "<A-k>", "<C-\\><C-N><C-w>k")
-vim.keymap.set({"t", "i"}, "<A-l>", "<C-\\><C-N><C-w>l")
-vim.keymap.set("n", "<A-h>", "<C-w>h")
-vim.keymap.set("n", "<A-j>", "<C-w>j")
-vim.keymap.set("n", "<A-k>", "<C-w>k")
-vim.keymap.set("n", "<A-l>", "<C-w>l")
-vim.keymap.set("n", "<A-H>", "<C-w>H")
-vim.keymap.set("n", "<A-J>", "<C-w>J")
-vim.keymap.set("n", "<A-K>", "<C-w>K")
-vim.keymap.set("n", "<A-L>", "<C-w>L")
+vim.opt.langmap='hjklHJKLneioNEIO;neioNEIOhjklHJKL'
+vim.keymap.set('n', ';', ':')
+vim.keymap.set({'t', 'i'}, '<A-h>', '<C-\\><C-N><C-w>h')
+vim.keymap.set({'t', 'i'}, '<A-j>', '<C-\\><C-N><C-w>j')
+vim.keymap.set({'t', 'i'}, '<A-k>', '<C-\\><C-N><C-w>k')
+vim.keymap.set({'t', 'i'}, '<A-l>', '<C-\\><C-N><C-w>l')
+vim.keymap.set({'t', 'i'}, '<A-h>', '<C-\\><C-N><C-w>h')
+vim.keymap.set({'t', 'i'}, '<A-j>', '<C-\\><C-N><C-w>j')
+vim.keymap.set({'t', 'i'}, '<A-k>', '<C-\\><C-N><C-w>k')
+vim.keymap.set({'t', 'i'}, '<A-l>', '<C-\\><C-N><C-w>l')
+vim.keymap.set('n', '<A-h>', '<C-w>h')
+vim.keymap.set('n', '<A-j>', '<C-w>j')
+vim.keymap.set('n', '<A-k>', '<C-w>k')
+vim.keymap.set('n', '<A-l>', '<C-w>l')
+vim.keymap.set('n', '<A-H>', '<C-w>H')
+vim.keymap.set('n', '<A-J>', '<C-w>J')
+vim.keymap.set('n', '<A-K>', '<C-w>K')
+vim.keymap.set('n', '<A-L>', '<C-w>L')
 vim.opt.startofline=true
 vim.opt.cdhome=true
 vim.opt.ignorecase=true
@@ -27,36 +27,36 @@ vim.opt.sidescroll=16
 vim.opt.sidescrolloff=2
 vim.opt.number=true
 vim.opt.relativenumber=true
-vim.opt.clipboard="unnamedplus"
+vim.opt.clipboard='unnamedplus'
 vim.opt.undofile=true
 vim.opt.tabstop=2
 vim.opt.shiftwidth=2
 vim.opt.smartindent=true
 
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+  local out = vim.fn.system({ 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath })
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
+      { 'Failed to clone lazy.nvim:\n', 'ErrorMsg' },
+      { out, 'WarningMsg' },
+      { '\nPress any key to exit...' },
     }, true, {})
     vim.fn.getchar()
     os.exit(1)
   end
 end
 vim.opt.rtp:prepend(lazypath)
-vim.g.mapleader = " "
+vim.g.mapleader = ' '
 
 local servers = {'clangd', 'gopls', 'lua_ls', 'pylsp'}
 -- Setup lazy.nvim
-require("lazy").setup({
+require('lazy').setup({
   spec = {
     {
       'nvim-treesitter/nvim-treesitter',
-      version = "*",
+      version = '*',
       config = function()
         require('nvim-treesitter.configs').setup{
           highlight = {
@@ -67,7 +67,7 @@ require("lazy").setup({
     },
     {
       'neovim/nvim-lspconfig',
-      version = "*",
+      version = '*',
       config = function()
         local lsp = require('lspconfig')
         for _, server in ipairs(servers) do
@@ -78,7 +78,7 @@ require("lazy").setup({
     {
       'nvim-telescope/telescope.nvim',
       requires = { 'nvim-lua/plenary.nvim' },
-      version = "*",
+      version = '*',
       config = function()
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
@@ -152,25 +152,25 @@ require("lazy").setup({
     },
     {
       'windwp/nvim-autopairs',
-      event = "InsertEnter",
+      event = 'InsertEnter',
       config = true
       -- use opts = {} for passing setup options
       -- this is equalent to setup({}) function
     },
     {
       'kylechui/nvim-surround',
-      version = "*", -- Use for stability; omit to use `main` branch for the latest features
-      event = "VeryLazy",
+      version = '*', -- Use for stability; omit to use `main` branch for the latest features
+      event = 'VeryLazy',
       config = function()
-          require("nvim-surround").setup({
+          require('nvim-surround').setup({
               -- Configuration here, or leave empty to use defaults
           })
       end
     },
     {
-      "ggandor/leap.nvim",
+      'ggandor/leap.nvim',
       lazy = false,
-      version = "*",
+      version = '*',
       config = function()
         require('leap').create_default_mappings()
       end
@@ -188,22 +188,22 @@ require("lazy").setup({
       end,
     },
     {
-      "nvim-neorg/neorg",
+      'nvim-neorg/neorg',
       lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
-      version = "*", -- Pin Neorg to the latest stable release
+      version = '*', -- Pin Neorg to the latest stable release
       config = true,
     },
     {
-      "NeogitOrg/neogit",
+      'NeogitOrg/neogit',
       dependencies = {
-        "nvim-lua/plenary.nvim",         -- required
+        'nvim-lua/plenary.nvim',         -- required
       },
       config = true
     },
     {
       'catppuccin/nvim',
       config = function()
-        require("catppuccin").setup({
+        require('catppuccin').setup({
           transparent_background = true,
         })
       end,
@@ -218,25 +218,25 @@ require("lazy").setup({
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { "habamax" } },
+  install = { colorscheme = { 'habamax' } },
   -- automatically check for plugin updates
   checker = { enabled = false },
 })
 
-vim.cmd("colorscheme catppuccin-macchiato")
-vim.keymap.set("n", "<leader>q", ":q<CR>")
-vim.keymap.set("n", "<leader>Q", ":q!<CR>")
-vim.keymap.set("n", "<leader>w", ":w<CR>")
-vim.keymap.set("n", "<leader>W", ":wq<CR>")
-vim.keymap.set("n", "<leader>e", ":e ")
-vim.keymap.set("n", "<leader>t", ":tabnew<CR>")
-vim.keymap.set("n", "<leader>x", ":bdelete<CR>")
-vim.keymap.set("n", "<leader>l", ":vsp<CR><C-w>l")
-vim.keymap.set("n", "<leader>j", ":sp<CR><C-w>j")
-vim.keymap.set("n", "<leader>c", ":term<CR>")
-vim.keymap.set("n", "<leader>g", ":Neogit<CR>")
-vim.keymap.set("n", "<leader>n", ":bn<CR>")
-vim.keymap.set("n", "<leader>p", ":bp<CR>")
-vim.keymap.set("n", "<leader>m", ":marks<CR>")
-vim.keymap.set("n", "<leader>d", vim.lsp.buf.definition)
-vim.keymap.set("n", "<leader>r", vim.lsp.buf.references)
+vim.cmd('colorscheme catppuccin-macchiato')
+vim.keymap.set('n', '<leader>q', ':q<CR>')
+vim.keymap.set('n', '<leader>Q', ':q!<CR>')
+vim.keymap.set('n', '<leader>w', ':w<CR>')
+vim.keymap.set('n', '<leader>W', ':wq<CR>')
+vim.keymap.set('n', '<leader>e', ':e ')
+vim.keymap.set('n', '<leader>t', ':tabnew<CR>')
+vim.keymap.set('n', '<leader>x', ':bdelete<CR>')
+vim.keymap.set('n', '<leader>l', ':vsp<CR><C-w>l')
+vim.keymap.set('n', '<leader>j', ':sp<CR><C-w>j')
+vim.keymap.set('n', '<leader>c', ':term<CR>')
+vim.keymap.set('n', '<leader>g', ':Neogit<CR>')
+vim.keymap.set('n', '<leader>n', ':bn<CR>')
+vim.keymap.set('n', '<leader>p', ':bp<CR>')
+vim.keymap.set('n', '<leader>m', ':marks<CR>')
+vim.keymap.set('n', '<leader>d', vim.lsp.buf.definition)
+vim.keymap.set('n', '<leader>r', vim.lsp.buf.references)
