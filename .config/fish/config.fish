8445a72ae12c637ddd2f9fdc9d401a0c4989ef00
Rock Clapps <rockclapps@gmail.com>
2024-08-22 09:51:35 -0400

    Cleaned up

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,10 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     source ~/.bash_aliases
-    thefuck --alias | source
-    starship init fish | source
+    if test (type -p starship != '')
+      starship init fish | source
+    end
+    if test (type -p zoxide != '')
+      zoxide init fish | source
+    end
 end
-
-
