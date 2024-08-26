9617d8e5380b9384ed7bc659cf682c3a31444698
Rock Clapps <rockclapps@gmail.com>
2024-08-26 09:57:31 -0400

    updated fish variables

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,7 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    set -gx GOPATH "$HOME/go"
+    set -gx PATH "$PATH:$GOPATH/bin"
     source ~/.aliases
     if test (type -p starship != '')
       starship init fish | source
