258c71bcbd61da00a6be86af4e0a2cf779cd34f7
Rock <rock@syn-2603-7081-5200-e8f1-0000-0000-0000-10a3.res6.spectrum.com>
2025-05-10 00:42:19 -0400

    fedora atomic adjustments

--- scripts/ollama-stack.sh
+++ scripts/ollama-stack.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -d -p 3000:8080 -e WEBUI_AUTH=False -v ollama:/root/.ollama -v open-webui:/app/backend/data --replace --name open-webui --restart always ghcr.io/open-webui/open-webui:ollama
+podman run -d -p 8080:8080 -e WEBUI_AUTH=False -v ollama:/root/.ollama -v open-webui:/app/backend/data --replace --name open-webui --restart always ghcr.io/open-webui/open-webui:ollama
