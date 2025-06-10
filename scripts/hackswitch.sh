16cdefafd7668f26e8601e3c0779bf60a02c082a
Rock Clapps <rockclapps@gmail.com>
2025-06-10 12:36:44 -0400

    moved files

--- scripts/hackswitch.sh
+++ scripts/hackswitch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo python3 "$HOME/MEGA/Game Saves/Switch Hacking/fusee-launcher-1.0/fusee-launcher.py" "$HOME/MEGA/Game Saves/Switch Hacking/fusee-launcher-1.0/pls/payload.bin"
+sudo python3 "$HOME/CloudFiles/Game Saves/Switch Hacking/fusee-launcher-1.0/fusee-launcher.py" "$HOME/CloudFiles/Game Saves/Switch Hacking/fusee-launcher-1.0/pls/payload.bin"
