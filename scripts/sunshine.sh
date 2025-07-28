41a23d330275ee25855ca26ae913baeae1dbe65c
Rock Clapps <rockclapps@gmail.com>
2025-07-27 20:09:42 -0400

    script for faster internet during sunshine

--- scripts/sunshine.sh
+++ scripts/sunshine.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 kscreen-doctor output.eDP-2.mode.1920x1080@60
+nmcli device modify wlp6s0 wifi.powersave 2
+systemctl restart NetworkManager
 DRI_PRIME=1 sunshine
 kscreen-doctor output.eDP-2.mode.1920x1200@165
+nmcli device modify wlp6s0 wifi.powersave 3
+systemctl restart NetworkManager
